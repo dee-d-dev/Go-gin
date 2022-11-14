d10bcbe769c1b9cd1f3beb5de9982a31e79f4b55
dee-d-dev <adedigbaadedotunemmanuel@gmail.com>
2022-11-14 05:04:18 +0100

    WIP

--- main.go
+++ main.go
@@ -2,10 +2,14 @@ package main
 
 import (
 	"context"
+	"fmt"
+	"log"
 
+	"github.com/dee-d-dev/go-gin/controllers"
 	"github.com/dee-d-dev/go-gin/services"
 	"github.com/gin-gonic/gin"
-	"go.mongodb.org/mongo-driver/mongo" 
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 var (
@@ -18,11 +22,40 @@ var (
 	err            error
 )
 
-func init(){
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+
+	}
+	MONGO_URI := os.GetEnv("MONGO_URL")
 	ctx = context.TODO()
 
-	mongoconn := optiosns.Client().ApplyURI()
+	mongoconn := options.Client().ApplyURI(MONGO_URI)
+	mongoclient, err := mongo.Connect(ctx, mongoconn)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err = mongoclient.Ping(ctx, redpref.Primary()); err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("mongodb has been established")
+
+	usercollection = mongoclient.Database("go-gin").Collection("users")
+	userservice = services.NewUserService(usercollection, ctx)
+	usercontroller = controllers.New(userservice)
+	server = gin.Default()
+
 }
 func main() {
+	defer mongoclient.Disconnect(ctx)
+	basepath := server.Group("/v1")
+
+	usercontroller.RegisterUserRoutes(basepath)
+
+	log.Fatal(server.Run(":9090"))
 
 }
